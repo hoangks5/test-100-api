ab43ec52659eefddae3155e88054f1055da5e791
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2021-11-29 14:55:47 +0700

    commit

--- up.py
+++ up.py
@@ -3,7 +3,7 @@ from random import randint
 
 for i in range(40,700):
     
-    for j in range(0, randint(1,10)):
+    for j in range(0, randint(200,500)):
         d = str(i) + ' day ago'
         with open('file.txt','a') as file:
             file.write(d)
