b286f92d089f3c6b9bb974278f67cec29d132a4f
Nguyễn Đức Hoàng <81577195+hoangks5@users.noreply.github.com>
2022-05-16 16:11:19 +0700

    commit

--- up.py
+++ up.py
@@ -5,7 +5,7 @@ for i in range(40,700):
     
     for j in range(0, randint(200,500)):
         d = str(i) + ' day ago'
-        with open('file1.txt','a') as file:
+        with open('file.txt','a') as file:
             file.write(d)
         os.system('git add .')
         os.system('git commit --date="'+d+'" -m "commit"')
